3820f9e97309df628bc3bf532623e4e66acf5156
Aayushi <aashi74054@gmail.com>
2021-01-12 14:07:11 +0530

    route reducer

--- ata-sprint-react/src/reducers/route_reducer.js
+++ ata-sprint-react/src/reducers/route_reducer.js
@@ -5,6 +5,10 @@ const productReducer = (state=initialState, action) => {
             return action.payload;
         case 'VIEW_ROUTE':
             return action.payload;
+        case 'DELETE_ROUTE':
+            return action.payload;
+        case 'EDIT_ROUTE':
+            return action.payload;
         default:
             return [];
     }
