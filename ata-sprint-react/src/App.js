ff2f968805a3904293ef2ad52dabb4d3edb63acb
Aayushi <aashi74054@gmail.com>
2021-01-12 10:21:12 +0530

    update_route

--- ata-sprint-react/src/App.js
+++ ata-sprint-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { AddRouteComponent } from './components/add_route'
 import { AddVehicleComponent } from './components/add_vehicle'
+import { UpdateRouteComponent } from './components/update_route';
 
 function App() {
   return (
     <div className="App">
-        <AddVehicleComponent/>
+        {/*<AddVehicleComponent>*/}
       {/* <AddRouteComponent/> */}
+      <UpdateRouteComponent/>
     </div>
   );
 }
