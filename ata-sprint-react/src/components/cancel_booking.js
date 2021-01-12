100247d8937d8a74d919493d2a35d669e4022555
Swati <swatimsm14.sm@gmail.com>
2021-01-12 11:44:17 +0530

    Update_Cancel_Booking

--- ata-sprint-react/src/components/cancel_booking.js
+++ ata-sprint-react/src/components/cancel_booking.js
@@ -3,21 +3,19 @@ import '../css/cancel_booking.css'
 export const CancelBookingComponent = (props) =>{
 
    return(
+     <body>
        <div class="testbox">
-    <form action="/"></form>
+    <form action="/">
       <div class="banner">
         <h1>Cancel Booking</h1>
       </div>
       <div class="item"></div>
         <p>Booking Id</p>
-        <div class="Id">  <input type="text" name="id" placeholder="Enter your id" />
+        <div class="Id">  
+        <input type="text" name="id" placeholder="Enter your id" />
           <input type="submit" name="submit" class="submit" value="Search"/>
           </div>
-         
-         
-        
-
-      <div class="item">
+         <div class="item">
         <p>Boarding Point</p>
         <select required>
           <option value="1">Mumbai</option>
@@ -54,6 +52,8 @@ export const CancelBookingComponent = (props) =>{
         <div class="btn-block">
           <button type="submit" href="/">Cancel Booking</button>
         </div>
+        </form>
         </div>
+        </body>
    );
       }
