028e319cac135b237ac9dae556aa45c97d1a5417
Sakshi Tanurkar <sakshi.tanurkar@gmail.com>
2021-01-12 09:49:25 +0530

    Add Driver

--- ata-sprint-react/src/App.js
+++ ata-sprint-react/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { AddRouteComponent } from './components/add_route'
 import { AddVehicleComponent } from './components/add_vehicle'
-
+import {AddDriverComponent} from './components/add_driver'
 function App() {
   return (
     <div className="App">
-        <AddVehicleComponent/>
+        {/*<AddVehicleComponent/>}
       {/* <AddRouteComponent/> */}
+      <AddDriverComponent/>
     </div>
   );
 }
