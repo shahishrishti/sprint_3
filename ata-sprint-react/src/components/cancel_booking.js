04b629125487b8443db77d3747247b1d71f4f908
Akshata <akshtaraut34@gmail.com>
2021-01-12 15:09:36 +0530

    Cancel_booking_js

--- ata-sprint-react/src/components/cancel_booking.js
+++ ata-sprint-react/src/components/cancel_booking.js
@@ -1,48 +1,33 @@
 import React from 'react';
+import '../css/cancel_booking.css'
+export const CancelBookingComponent = (props) =>{
 
-export class CancelBookingComponent extends React.Component {
-    render() {
+   return(
+     <body>
        <div class="testbox">
-    <form action="/"></form>
+    <form action="/">
       <div class="banner">
         <h1>Cancel Booking</h1>
       </div>
       <div class="item"></div>
         <p>Booking Id</p>
-        <div class="Id">  <input type="text" name="id" placeholder="Enter your id" />
+        <div class="Id">  
+        <input type="text" name="id" placeholder="Enter your id" />
           <input type="submit" name="submit" class="submit" value="Search"/>
           </div>
-         
-         
-        
-
-      <div class="item">
+         <div class="item">
         <p>Boarding Point</p>
-        <select required>
-          <option value="1">Mumbai</option>
-          <option value="2">Pune</option>
-          <option value="3">Nashik</option>
-          <option value="4">Nagpur</option>
-          <option value="5">Goa</option>
-          <option value="6">Surat</option>
-        </select>
+        <input type="text" name="boarding point"/>
       </div>
       
       <div class="item">
         <p>Drop Point</p>
-        <select required>
-          <option value="1">Surat</option>
-          <option value="2">Pune</option>
-          <option value="3">Mumbai</option>
-          <option value="4">Nagpur</option>
-          <option value="5">Goa</option>
-          <option value="6">Nashik</option>
-        </select>
+        <input type="text" name="drop point" />
       </div>
      
       <div class="item">
         <p>Journey Date</p>
-        <input type="date" id="journeydate" name="journeydate"></input>
+        <input type="date" name="journeydate" placeholder="enter the date"></input>
       </div>
 
       <div class="item">
@@ -53,6 +38,8 @@ export class CancelBookingComponent extends React.Component {
         <div class="btn-block">
           <button type="submit" href="/">Cancel Booking</button>
         </div>
+        </form>
         </div>
-        }
+        </body>
+   );
       }
