1e4a50c8d2b76a605014f778b327cee5ea810390
Richa <richaaware@gmail.com>
2021-01-12 14:32:24 +0530

    Driver

--- ata-sprint-react/src/App.js
+++ ata-sprint-react/src/App.js
@@ -1,75 +1,92 @@
 import './App.css';
-<<<<<<< HEAD
-import { ModifyDriverComponent } from './components/modify_driver';
-=======
-<<<<<<< HEAD
-import {AddBookingComponent} from './components/add_booking';
->>>>>>> 837dca2ae94b1ccd76fde247526f141ea7238bfe
-
-=======
+import { ModifyDriverComponent } from './components/modify_driver'
+import {AddBookingComponent} from './components/add_booking'
 import {  BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 import { AddRouteComponent } from './components/add_route'
-import { AddVehicleComponent } from './components/add_vehicle'
-<<<<<<< HEAD
-import {AddDriverComponent} from './components/add_driver'
-=======
-import { ViewRouteComponent } from './components/view_route'
-import { HomePageComponent } from './components/home_page'
->>>>>>> 5e2e53532f39081702517a950e971bbaf8ff128f
->>>>>>> f6d1676d7bfb93ce85eef0e3203e47c64d53b1ce
+import { AddVehicleComponent } from './components/add_vehicle';
+import {AddDriverComponent} from './components/add_driver';
+import { ViewRouteComponent } from './components/view_route';
+import { ViewVehicleComponent } from './components/view_vehicle';
+import { HomePageComponent } from './components/home_page';
+import { ViewBookingByNameAndStatusComponent } from './components/view_booking_by_name_and_status';
+import { ViewBookingByNameComponent } from './components/view_booking_by_name';
+import { CancelBookingComponent } from './components/cancel_booking';
+import { UpdateRouteComponent } from './components/update_route';
+import { ModifyVehicleComponent } from './components/modify_vehicle';
+import { ViewDriverComponent } from './components/view_driver';
+import { ViewBookingComponent } from "./components/view_booking";
+
 function App() {
   return (
-    <Router>
-    <div className="App">
-<<<<<<< HEAD
-      <ModifyDriverComponent></ModifyDriverComponent>
-=======
-<<<<<<< HEAD
-        {/*<AddVehicleComponent/>}
-      {/* <AddRouteComponent/> */}
-      <AddDriverComponent/>
-=======
-<<<<<<< HEAD
-      <AddBookingComponent/>
->>>>>>> f6d1676d7bfb93ce85eef0e3203e47c64d53b1ce
->>>>>>> 837dca2ae94b1ccd76fde247526f141ea7238bfe
-    </div>
-=======
-      <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/addroute">Add Route</Link>
-          </li>
-          <li>
-            <Link to="/viewroute">View Route</Link>
-          </li>
-          <li>
-            <Link to="/addvehicle">Add Vehicle</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-      <Switch>
-          <Route exact path="/">
-            <HomePageComponent />
-          </Route>
-          <Route path="/addroute">
-            <AddRouteComponent/>
-          </Route>
-          <Route path="/viewroute">
-            <ViewRouteComponent/>
-          </Route>
-          <Route path="/addvehicle">
-            <AddVehicleComponent/>
-          </Route>
-        </Switch> 
-        </div>       
-        </Router>
->>>>>>> 5e2e53532f39081702517a950e971bbaf8ff128f
-  );
-}
-
+      <Router>
+      <div className="App">
+        
+              <Link to="/">Home</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/viewroute">View Route</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/addroute">Add Route</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/viewvehicle">View Vehicle</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/addvehicle">Add Vehicle</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/addbooking">Add Booking</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/adddriver">Add Driver</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/editdriver">Edit Driver</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/viewbookingbyname">View Booking By Name</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/viewbookingbystatus">View Booking By Status</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/cancelbooking">Cancel Booking</Link>&nbsp;&nbsp;&nbsp;
+              <Link to="/editroute">Edit Route</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/editvehicle">Edit Vehicle</Link>&nbsp;&nbsp;&nbsp;
+              <Link to="/viewdriver">View Driver</Link> &nbsp;&nbsp;&nbsp;
+              <Link to="/booking">View Booking</Link>&nbsp;&nbsp;&nbsp;
+          <hr />
+  
+        <Switch>
+            <Route exact path="/">
+              <HomePageComponent />
+            </Route>
+            <Route path="/viewroute">
+              <ViewRouteComponent/>
+            </Route>
+            <Route path="/addroute">
+              <AddRouteComponent/>
+            </Route>
+            <Route path="/viewvehicle">
+              <ViewVehicleComponent/>
+            </Route>
+            <Route path="/addvehicle">
+              <AddVehicleComponent/>
+            </Route>
+            <Route path="/addBooking">
+              <AddBookingComponent/>
+            </Route>
+            <Route path="/adddriver">
+              <AddDriverComponent/>
+            </Route>
+            <Route path="/editdriver">
+              <ModifyDriverComponent/>
+            </Route>
+            <Route path="/viewbookingbyname">
+              <ViewBookingByNameComponent/>
+            </Route>
+            <Route path="/viewbookingbystatus">
+              <ViewBookingByNameAndStatusComponent/>
+            </Route>
+            <Route path="/cancelbooking">
+              <CancelBookingComponent/>
+            </Route>
+            <Route path="/editroute">
+              <UpdateRouteComponent/>
+            </Route>
+            <Route path="/editvehicle">
+              <ModifyVehicleComponent/>
+            </Route>
+            <Route path="/viewdriver">
+              <ViewDriverComponent/>
+            </Route>
+            <Route path="/booking">
+              <ViewBookingComponent/>
+            </Route>
+          </Switch> 
+          </div>       
+          </Router>
+    );
+  }
 export default App;
