29eab739ccf2ebfed1a41f3f8727b7eb1e9c8b34
Aayushi <aashi74054@gmail.com>
2021-01-13 12:45:13 +0530

    route

--- ata-sprint-react/src/actions/delete_route_action.js
+++ ata-sprint-react/src/actions/delete_route_action.js
@@ -1,8 +1,17 @@
-export const DeleteRouteAction =(route)=>{
-    return {
-        type: 'DELETE_ROUTE',
-        payload: route
-        }
-      }
-    
+import axios from 'axios';
+
+const DeleteRouteAction = (routeId) => {
+    return async function(dispatch) {
+        const res = await axios.delete(
+            "http://localhost:9090/cgata/route/" + routeId, { 
+                headers: { 
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            });
+          console.log('Delete Route serverResponse: ', res.data);
+          dispatch({type: "DELETE_ROUTE", payload: res.data});
+
+    }
+}
+
 export default DeleteRouteAction;
