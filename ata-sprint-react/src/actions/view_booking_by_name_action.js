4288c650e676c3980663ad74b8ee3f69504d865b
Prathamesh <dhaboo1999@gmail.com>
2021-01-13 12:43:04 +0530

    viewBookingaction

--- ata-sprint-react/src/actions/view_booking_by_name_action.js
+++ ata-sprint-react/src/actions/view_booking_by_name_action.js
@@ -1,8 +1,17 @@
-const viewBookingByNameAction = (booking)=> {
-    return {
-      type: 'VIEW_BOOKING_BY_NAME',
-      payload: booking
-    }
-  }
+import axios from 'axios';
 
-  export default viewBookingByNameAction;
+let ViewBookingByNameAction = (name) => {
+    return async function(dispatch) {
+        const res = await axios.get(
+            "http://localhost:9090/cgata/booking/" + name , { 
+                headers: { 
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            });
+          console.log('View Booking serverResponse: ', res.data);
+          dispatch({type: "VIEW_BOOKING_BY_NAME", payload: res.data});
+
+    }
+}
+
+export default ViewBookingByNameAction;
