39a9692e1ba0f986885cceff81f5834735959c5c
Akshata <akshtaraut34@gmail.com>
2021-01-13 11:13:17 +0530

    cancel booking

--- ata-sprint-react/src/App.js
+++ ata-sprint-react/src/App.js
@@ -3,59 +3,6 @@ import { ModifyDriverComponent } from './components/modify_driver'
 import {AddBookingComponent} from './components/add_booking'
 import {  BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 import { AddRouteComponent } from './components/add_route'
-<<<<<<< HEAD
-import { AddVehicleComponent } from './components/add_vehicle'
-import { ViewRouteComponent } from './components/view_route'
-import { HomePageComponent } from './components/home_page'
-import { CancelBookingComponent} from './components/cancel_booking'
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/addroute">Add Route</Link>
-          </li>
-          <li>
-            <Link to="/viewroute">View Route</Link>
-          </li>
-          <li>
-            <Link to="/addvehicle">Add Vehicle</Link>
-          </li>
-          <li>
-            <Link to="/cancelbooking">Cancel Booking</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-      <Switch>
-          <Route exact path="/">
-            <HomePageComponent />
-          </Route>
-          <Route path="/addroute">
-            <AddRouteComponent/>
-          </Route>
-          <Route path="/viewroute">
-            <ViewRouteComponent/>
-          </Route>
-          <Route path="/addvehicle">
-            <AddVehicleComponent/>
-          </Route>
-          <Route path="/cancelbooking">
-            <CancelBookingComponent/>
-          </Route>
-        </Switch> 
-        </div>       
-        </Router>
-  );
-}
-
-export default App;
-=======
 import { AddVehicleComponent } from './components/add_vehicle';
 import {AddDriverComponent} from './components/add_driver';
 import { ViewRouteComponent } from './components/view_route';
