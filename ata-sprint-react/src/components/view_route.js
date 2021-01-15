fbf770d86056cbf4e8388f6a91cbffa8139ea881
Aayushi <aashi74054@gmail.com>
2021-01-15 10:50:02 +0530

    route

--- ata-sprint-react/src/components/view_route.js
+++ ata-sprint-react/src/components/view_route.js
@@ -9,6 +9,8 @@ import GetAllSourceDestinationAction from '../actions/view_all_source_destinatio
 import GetRouteBySourceAction from '../actions/view_by_source';
 import GetRouteByDestinationAction from '../actions/view_by_destination';
 import GetRouteBySourceDestinationAction from '../actions/view_by_source_destination';
+import DeleteRouteAction from '../actions/delete_route_action';
+import axios from 'axios';
 
 let dispatch;
 let count = 0;
@@ -97,6 +99,7 @@ export const ViewRouteComponent = (props) => {
         }
     }
 
+
     const renderTableData = (routeList) => {
         console.log("routeList: ", routeList);
         return routeList.map((route) => {
@@ -106,7 +109,8 @@ export const ViewRouteComponent = (props) => {
                     <td>{source}</td>
                     <td>{destination}</td>
                     <td>{distance}</td>
-                    <td><button type="submit" id="action" href="/">Edit</button><button type="submit" id="action" href="/">Delete</button></td>
+                    <td><button type="submit" id="action" href="/">Edit</button>
+                    <button onClick={props.deleteRoute.bind(this)} type="submit" id="action" href="/">Delete</button></td>
                 </tr>
             )
         });
