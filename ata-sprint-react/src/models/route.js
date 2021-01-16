5496100443ad5254a41237136baecef1f965ffc1
Shrishti <shahishrishti@gmail.com>
2021-01-16 18:03:24 +0530

    DEleted

--- ata-sprint-react/src/models/route.js
+++
@@ -1,9 +0,0 @@
-class Route {
-    constructor(source, destination, distance) {
-        this.source = source;
-        this.destination = destination;
-        this.distance = distance;
-    }
-}
-
-export default Route;
