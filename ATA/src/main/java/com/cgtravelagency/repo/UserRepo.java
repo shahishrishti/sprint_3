663efd9dc0f08551f0b087d8e3b05b6e046b342a
Shrishti <shahishrishti@gmail.com>
2021-01-17 16:22:38 +0530

    Delete

--- ATA/src/main/java/com/cgtravelagency/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cgtravelagency.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cgtravelagency.entity.UserEntity;
-
-public interface UserRepo extends JpaRepository<UserEntity, Long>{
-
-}
--- ata-sprint-react/src/components/home_page.js
+++
@@ -1,5 +0,0 @@
-export const HomePageComponent = (props) => {
-    return (
-        <h1>Automated Travel Agency</h1>
-    );
-}
